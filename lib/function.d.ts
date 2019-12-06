0089697261bbd049a9dc9352f7d58b0bba77c71c
zou wendi <542095059@qq.com>
2019-12-06 15:46:29 +0800

    修复类型bug

---
+++ lib/function.d.ts
@@ -0,0 +1,4 @@
+export declare function safeFunc(expr: string, args?: any[]): Function;
+export declare function createFunc(expr: string, args?: any[], ctx?: {
+    [key: string]: any;
+}): Function;
